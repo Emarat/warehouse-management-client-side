70226f7ca66d2d64e5679b739a235a12551cee97
Emarat <emarathossain00@gmail.com>
2022-05-08 00:41:01 +0600

    Restock item

--- src/Components/Inventory/Inventory.js
+++ src/Components/Inventory/Inventory.js
@@ -34,6 +34,28 @@ const Inventory = () => {
         setRefresh(!Refresh)
     };
 
+    const Restock = () => {
+        const inputValue = parseInt(document.getElementById("input").value);
+        console.log(inputValue);
+        const quantity = item.quantity + inputValue;
+        const updateQty = { quantity };
+        console.log(quantity);
+        const url = `http://localhost:5000/fruit/${id}`;
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(updateQty)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            })
+        // window.location.reload(false);
+        setRefresh(!Refresh)
+    }
+
     return (
         <div className='container mt-5'>
             <div className="card mb-3">
@@ -45,8 +67,14 @@ const Inventory = () => {
                     <p>Supplier: <small>{item.supplier}</small> </p>
                     <p>Quantity: <small>{item.quantity}</small> </p>
                     <p>Price: <small>{item.price}</small> </p>
-                    <div className='text-center'>
-                        <button onClick={Delivered} type="button" className="btn btn-primary">DELIVERED</button>
+                    <div className='d-flex'>
+                        <div>
+                            <button onClick={Delivered} type="button" className="btn btn-primary">DELIVERED</button>
+                        </div>
+                        <div className='d-flex mx-auto'>
+                            <input style={{ width: '50px' }} type="text" className="form-control" id="input" />
+                            <button onClick={Restock} type="button" className="btn btn-primary ms-3">RESTOCK</button>
+                        </div>
                     </div>
                 </div>
             </div>
