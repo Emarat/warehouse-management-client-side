97194b36589e96103f7ff276b86012ae49624891
Emarat <emarathossain00@gmail.com>
2022-05-08 08:51:03 -1100

    index.html

--- src/Components/Newsletter/Newsletter.js
+++ src/Components/Newsletter/Newsletter.js
@@ -5,7 +5,7 @@ const Newsletter = () => {
         <div className='container mt-5'>
             <section >
                 <div >
-                    <div class="container">
+                    <div className="container">
                         <h1 >Subscribe To Get The Latest  News About Us </h1>
                         <p >Get the Latest news about digital Marketing to Your Pocket, drop your email below to
                             get daliy update about us</p>
