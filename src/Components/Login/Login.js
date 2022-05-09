02510cb8c2fe6e967ad82533964b4c8705f405a6
Emarat <emarathossain00@gmail.com>
2022-05-09 17:40:25 +0600

    social login fixed

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -82,7 +82,7 @@ const Login = () => {
                                         <div className="d-flex justify-content-center text-center mt-4 pt-1">
                                             <a href="#!" className="text-white"><i className="fab fa-facebook-f fa-lg"></i></a>
                                             <a href="#!" className="text-white"><i className="fab fa-twitter fa-lg mx-4 px-2"></i></a>
-                                            <a onClick={() => signInWithGoogle()} href="#!" className="text-white"><i className="fab fa-google fa-lg"></i></a>
+                                            <a onClick={() => signInWithGoogle()} className="text-white"><i className="fab fa-google fa-lg"></i></a>
                                         </div>
 
                                     </div>
